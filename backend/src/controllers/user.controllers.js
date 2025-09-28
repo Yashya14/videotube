7042bece3d5a9d0bea684de89e20bd9c97d05ff7
Yash Pal <108535139+Yashya14@users.noreply.github.com>
2025-09-28 14:48:57 +0530

    fix: update refreshToken to a default value on logout

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -179,8 +179,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     userId,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1, // this removes the field from the document
       },
     },
     {
